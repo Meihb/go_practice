f66041dfb8e6c308827bd5a319e712ac01c86b34
harbor.M <1253880904@qq.com>
2019-12-30 16:52:07 +0800

    1

--- src/main/InterfaceInnerTheory.go
+++ src/main/InterfaceInnerTheory.go
@@ -1,5 +1,7 @@
 package main
 
+import "testing"
+
 /*
 非空接口底层iface
 //src/runtime/runtime2.go
@@ -63,6 +65,84 @@ func (adder Adder) Add(a, b int) int { return a + b }
 
 //go:noinline
 func (adder Adder) Sub(a, b int) int { return a - b }
+
+type identifier interface {
+	idInline() int32
+	idNoInline() int32
+}
+type id32 struct{ id int32 }
+
+func (id *id32) idInline() int32 { return id.id }
+
+//go:noinline
+func (id *id32) idNoInline() int32 { return id.id }
+
+var escapeMePlease *id32
+
+//主要作用是强制变量内存在 heap 上分配
+//go:noinline
+func escapeToHeap(id *id32) identifier {
+	escapeMePlease = id
+	return escapeMePlease
+}
+
+//直接调用
+func BenchmarkMethodCall_direct(b *testing.B) { //
+	var myID int32
+	b.Run("single/noinline", func(b *testing.B) {
+		m := escapeToHeap(&id32{id: 6754}).(*id32)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			//CALL "".(*id32).idNoinline(SB)
+			//MOVL 8(SP), AX
+			//MOVQ "".&myID+40(SP), CX
+			//MOVL AX, (CX)
+			myID = m.idNoInline()
+		}
+	})
+	b.Run("single/inline", func(b *testing.B) {
+		m := escapeToHeap(&id32{id: 6754}).(*id32)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			//MOVL (DX), SI
+			//MOVL SI, (CX)
+			myID = m.idInline()
+		}
+	})
+}
+
+//接口调用
+func BenchmarkMethodCall_interface(b *testing.B) { //
+	var myID int32
+	b.Run("single/noinline", func(b *testing.B) {
+		m := escapeToHeap(&id32{id: 6754})
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			// MOVQ 32(AX), CX
+			// MOVQ "".m.data+40(SP), DX
+			// MOVQ DX, (SP)
+			// CALL CX
+			// MOVL 8(SP), AX
+			// MOVQ "".&myID+48(SP), CX
+			// MOVL AX, (CX)
+			myID = m.idNoInline()
+		}
+	})
+	b.Run("single/inline", func(b *testing.B) {
+		m := escapeToHeap(&id32{id: 6754})
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			//MOVQ 24(AX), CX
+			//MOVQ "".m.data+40(SP), DX
+			//MOVQ DX, (SP)
+			//CALL CX
+			//MOVL 8(SP), AX
+			//MOVQ "". &myID+48(SP), ex
+			//MOVL AX, (CX)
+			myID = m.idInline()
+		}
+	})
+} //
 func main() {
 	var m Caler = Adder{id: 1234}
 	m.Add(10, 32)
