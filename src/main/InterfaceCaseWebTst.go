860aa16dc51bd96130e2c026371b7620da3e9ac8
harbor.M <1253880904@qq.com>
2020-01-02 17:38:37 +0800

    1

--- src/main/InterfaceCaseWebTst.go
+++ src/main/InterfaceCaseWebTst.go
@@ -1,2 +1,21 @@
-
 package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// 在/后面加上 index ，来指定访问路径
+	http.HandleFunc("/index", index)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+func index(w http.ResponseWriter, r *http.Request) {
+	content, error := ioutil.ReadFile("./src/main/index.html")
+	if error != nil {
+		fmt.Println(error)
+	}
+	w.Write(content)
+}
