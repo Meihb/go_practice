0477d789e49e11f7d8ca14151319783e58c89fd0
harbor.M <1253880904@qq.com>
2020-01-08 17:04:47 +0800

    1

---
+++ src/main/PackageTst.go
@@ -0,0 +1,12 @@
+package main
+import (
+	"fmt"
+	"model"
+)
+func main() {
+	p := model.NewPerson("smith")
+	p.SetAge(18)
+	p.SetSal(5000)
+	fmt.Println(p)
+	fmt.Println(p.Name, " age =", p.GetAge(), " sal = ", p.GetSal())
+}
